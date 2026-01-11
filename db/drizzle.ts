4df5dfd2b0c10caa3d549a59897e70e58163ee81
Said Mendoza <114017329+SaidMg10@users.noreply.github.com>
2026-01-10 22:46:48 -0500

    fix: Drizzle adapter changed from neonpg to pg

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,6 +1,12 @@
 import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { schema } from "./schema";
 
 config({ path: ".env" }); // or .env.local
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
