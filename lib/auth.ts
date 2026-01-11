82217e586e4312d5e44386f3df4e235151e9808c
Said Mendoza <114017329+SaidMg10@users.noreply.github.com>
2026-01-10 23:57:42 -0500

    fix: adding default value in auth.ts of better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,34 +5,35 @@ import { jwt } from "better-auth/plugins";
 import { schema } from "@/db/schema";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
-    schema: schema,
-  }),
-  emailAndPassword: {
-    enabled: true,
-  },
-  user: {
-    additionalFields: {
-      role: {
-        type: "string",
-        required: true,
-      },
-    },
-  },
-  plugins: [
-    jwt({
-      jwt: {
-        definePayload: ({ user }) => {
-          return {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-            role: user.role,
-          };
-        },
-      },
-    }),
-  ],
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+	database: drizzleAdapter(db, {
+		provider: "pg", // or "mysql", "sqlite"
+		schema: schema,
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	user: {
+		additionalFields: {
+			role: {
+				type: "string",
+				defaultValue: "user",
+				required: true,
+			},
+		},
+	},
+	plugins: [
+		jwt({
+			jwt: {
+				definePayload: ({ user }) => {
+					return {
+						id: user.id,
+						email: user.email,
+						name: user.name,
+						role: user.role,
+					};
+				},
+			},
+		}),
+	],
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 });
